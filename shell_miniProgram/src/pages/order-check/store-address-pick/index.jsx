453c2695e3c4ab23af6ecb7afcccde602f657584
zhonghaixiao <zhonghaixiao@madingyu.onaliyun.com>
2020-07-06 01:20:19 +0800

    zhx

--- shell_miniProgram/src/pages/order-check/store-address-pick/index.jsx
+++ shell_miniProgram/src/pages/order-check/store-address-pick/index.jsx
@@ -1,14 +1,35 @@
 import Taro, { PureComponent } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import icons from '@assets'
+import { ClText, ClAvatar } from "mp-colorui";
 
 import './index.scss'
+
 
 class Index extends PureComponent {
 
     render() {
+
+        // const { store } = this.props
+        const store = {
+            Name: '贝壳口腔',
+            Banner: [{ url: 'http://cdn.shuaixiaoxiao.com/image/20200706004653.jpg' }],
+            Address: '江西赣州市章贡区赣康路与锦江路交汇处中央公园首府7栋17号商铺'
+        }
+
         return (
-            <View>
-                门店地址
+            <View className='store'>
+                <View className='store__banner'>
+                    <ClAvatar headerArray={store.Banner} shape='round' size='small' />
+                </View>
+                <View className='store__info'>
+                    <ClText text={store.Name} size='normal' />
+                    <ClText text={store.Address} size='small' />
+                    <ClText text='下单后请到以上地址接受服务' textColor='orange' size='small' />
+                </View>
+                <View className='store__more-tip'>
+                    <Image className='icon' src={icons.more} />
+                </View>
             </View>
         )
     }
--- shell_miniProgram/src/pages/order-check/user-info/index.jsx
+++ shell_miniProgram/src/pages/order-check/user-info/index.jsx
@@ -1,5 +1,6 @@
 import Taro, { PureComponent } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { ClInput } from "mp-colorui";
 
 import './index.scss'
 
@@ -7,8 +8,9 @@ class Index extends PureComponent {
 
     render() {
         return (
-            <View>
-                用户信息
+            <View className='userinfo'>
+                <ClInput titleWidth={200} title="取货人" placeholder="请输入姓名" />
+                <ClInput titleWidth={200} title="联系电话" placeholder="请输入手机号" />
             </View>
         )
     }
