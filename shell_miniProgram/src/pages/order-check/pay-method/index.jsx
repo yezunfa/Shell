0cc7c83b3edca62572e376e8a73fc77d993145c3
zhonghaixiao <zhonghaixiao@madingyu.onaliyun.com>
2020-07-09 11:16:22 +0800

    zhx

--- shell_miniProgram/src/pages/order-check/pay-method/index.jsx
+++ shell_miniProgram/src/pages/order-check/pay-method/index.jsx
@@ -1,14 +1,36 @@
 import Taro, { PureComponent } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Image } from '@tarojs/components'
+import { pay_method } from '@constants/enums'
+import { ClText } from "mp-colorui";
+import icons from '@assets'
 
 import './index.scss'
 
+const baseClass = 'payComponent'
 class Index extends PureComponent {
 
     render() {
         return (
-            <View>
-                支付方法
+            <View className={`${baseClass}`}>
+                <View className={`${baseClass}__title`}>
+                    <ClText text='支付方式' size='normal' textColor='black' />
+                </View>
+                <View className={`${baseClass}__pay`}>
+                    {pay_method && pay_method.map(item => 
+                        <View className={`${baseClass}__pay-item`}>
+                            <View className={`${baseClass}__pay-item-icon`}>
+                                <Image className='icon' mode='aspectFit' src={icons.wechartPay} />
+                                <View className={`${baseClass}__pay-item-icon-text`}>
+                                    <ClText text={item.title} size='normal' textColor='black' />
+                                </View>
+                            </View>
+                            <View className={`${baseClass}__pay-item-right`}>
+                                <Image className='icon' src={icons.choose} />
+                                {/* <Image className='icon' src={icons.dis_choose} /> */}
+                            </View>
+                        </View>    
+                    )}
+                </View>
             </View>
         )
     }
--- shell_miniProgram/src/pages/order-check/user-info/index.jsx
+++ shell_miniProgram/src/pages/order-check/user-info/index.jsx
@@ -9,8 +9,12 @@ class Index extends PureComponent {
     render() {
         return (
             <View className='userinfo'>
-                <ClInput titleWidth={200} title="取货人" placeholder="请输入姓名" />
-                <ClInput titleWidth={200} title="联系电话" placeholder="请输入手机号" />
+                <View className='theme-title'>
+                    <ClInput titleWidth={200} title="取货人" placeholder="请输入姓名" />
+                </View>
+                <View>
+                    <ClInput titleWidth={200} title="联系电话" placeholder="请输入手机号" />
+                </View>
             </View>
         )
     }
