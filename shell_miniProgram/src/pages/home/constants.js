bd9bdfec511d5a3545e3d8b9dd474f60bde10fae
zhonghaixiao <zhonghaixiao@madingyu.onaliyun.com>
2021-01-17 17:32:50 +0800

    zhx

---
+++ shell_miniProgram/src/pages/home/constants.js
@@ -0,0 +1,8 @@
+export const ListNavGridTo = [
+    { type: 'page', url: null, message: '暂未开放' },
+    { type: 'tab', url: '/pages/cate/cate', message: '暂未开放' },
+    { type: 'tab', url: '/pages/cate/cate', message: '暂未开放' },
+    { type: 'tab', url: '/pages/user/user', message: '暂未开放' },
+    { type: 'page', url: null, message: '暂未开放' },
+    { type: 'page', url: null, message: '暂未开放' },
+]
